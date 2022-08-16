464ba8a5b0e33f114b70390a84d155a953a7162b
Ola Ajibode <ola.ajibode@gmail.com>
2022-08-16 20:08:05 +0100

    Feat: Add dever to response

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json({ message: 'CICD app', version: '0.0.2' });
+  res.json({ message: 'CICD app', version: '0.0.3',
+developed: 'Ola Ajibode' });
 });
 
 app.listen(3000, () => console.log(`Listening on port http://localhost:3000`));
