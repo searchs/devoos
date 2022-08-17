8fd5ce83df5fab2f76c63987c67c4093f4f57c6d
Ola Ajibode <ola.ajibode@gmail.com>
2022-08-17 11:57:01 +0100

    Feat: Add date to echo

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json({ message: 'CICD app', version: '0.0.3',
-developed: 'Ola Ajibode' });
+  res.json({ 
+    message: 'CICD app', 
+    version: '0.0.4', 
+    company: 'MarkTwo Platform' });
 });
 
 app.listen(3000, () => console.log(`Listening on port http://localhost:3000`));
